96b3d8775702592eed0058d7a22513348dffa2f1
hadeelyn <61551822+hadeelyn@users.noreply.github.com>
2023-03-21 12:28:33 +0200

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-package org.example;public class Main {
-=======
 package org.example;
 
 public class Main {
->>>>>>> 4889f06 (Initial commit)
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/test/java/TestPackage/JoinFormIGC.java
+++ src/test/java/TestPackage/JoinFormIGC.java
@@ -39,7 +39,3 @@ public class JoinFormIGC {
     }
 
 }
-class JoinFormIndividal{
-}
-class JoinFormComp{
-}
